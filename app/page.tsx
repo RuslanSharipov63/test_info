d0345bf17bbdd79c6135e4f6febc5d2c497869c3
unknown <sharipov.r@mail.ru>
2024-09-29 23:23:08 +0400

    title update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button"
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Link href="cars">Cars</Link>
+      <Link href="cars" className="flex justify-center mt-4 text-2xl"><Button variant="destructive">Cars</Button></Link>
     </div>
   );
 }
