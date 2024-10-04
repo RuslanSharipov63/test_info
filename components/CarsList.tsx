7901a1c85b72a498557063ee5bc79d7fcbad6f67
unknown <sharipov.r@mail.ru>
2024-10-04 22:44:28 +0400

    filters update2

--- components/CarsList.tsx
+++ components/CarsList.tsx
@@ -17,81 +17,37 @@ const CarList = () => {
 
   const searchFilters = (param: FilterStoreForStateType[]) => {
     const newArr: FilterStoreForStateType[] = [];
-    let newArr2: Cars[] = [];
-    const newArr3: Cars[] = [];
+   let resultArr = []
 
-    param.map(value => {
+    param.map((value) => {
       if (value.checked) {
-        newArr.push(value)
+        newArr.push(value);
       }
-    })
-    newArr2 = sortFilter(newArr, carsStore);
+    });
+    resultArr = sortFilter(newArr, carsStore);
 
-  setCarsData([...newArr2]);
+    setCarsData([...resultArr]);
 
-
-/*     if (newArr.length === 1) {
-      carsStore.map((item) => {
-        if (item.model === newArr[0]) {
-          const checkEl = newArr2.includes(item);
-          if (checkEl === false) {
-            newArr2.push(item);
-          }
-        }
-      });
-    }
- 
-    if (newArr.length > 1) {
-      carsStore.map((item) => {
-        let counter = 0;
-        newArr.map((el) => {
-          let checkElForCounter = Object.values(item).includes(el);
-          if (checkElForCounter) {
-            counter++;
-          }
-          if (counter === newArr.length) {
-            const checkEl = newArr2.includes(item);
-            if (checkEl === false) {
-              newArr2.push(item);
-            }
-          }
-        });
-      });
-    } */
-
-/* carsStore.map((item) => {
-  newArr.map((el) => {
-    if (
-     el.model === item.model
-    ) {
-      const checkEl = newArr2.includes(item);
-      if (checkEl === false) {
-        newArr2.push(item);
-      }
-
-    }
-  });
-}); 
-*/
-
-setCarsData([...newArr2]);
+   
   };
 
-return (
-  <>
-    <Filters searchFilters={searchFilters} />
-    {(carsData.length === 0 && checkAddFilter != true) &&
-      <p className="flex justify-center">Загрузка...</p>
-    }
-    {checkAddFilter != false && <p className="flex justify-center">Совпадения не найдены.</p>}
-    <div className="grid sm:grid-cols-5 gap-4 m-4">
-      {carsData.length > 0 &&
-        carsData.map((el) => {
-          return <Car cars={el} key={el.id} />;
-        })}
-    </div>
-  </>
-);
+  return (
+    <>
+      <Filters searchFilters={searchFilters} />
+      {carsData.length === 0 && checkAddFilter != true && (
+        <p className="flex justify-center">Загрузка...</p>
+      )}
+      {checkAddFilter != false && (
+        <p className="flex justify-center">Совпадения не найдены.</p>
+      )}
+      <div className="grid sm:grid-cols-5 gap-4 m-4">
+        {carsData.length > 0 &&
+          carsData.map((el) => {
+            return <Car cars={el} key={el.id} />;
+          })}
+      </div>
+    </>
+  );
 };
 
 export default CarList;
--- helpers/sortFilter.ts
+++ helpers/sortFilter.ts
@@ -1,23 +1,80 @@
 import { FilterStoreForStateType } from "@/type";
 import { Cars } from "@/type";
 
-export const sortFilter = (checkFilter: FilterStoreForStateType[], afterArr: Cars[]) => {
-    let resultArr: Cars[] = [];
-    checkFilter.map(el => {
-        if (el.model) {
-            afterArr.map(item => {
-                if (el.model === item.model) {
-                    const checkEl = resultArr.includes(item);
-                    if (checkEl === false) {
-                        resultArr.push(item);
-                    }
-                }
-            })
+export const sortFilter = (
+  checkFilter: FilterStoreForStateType[],
+  afterArr: Cars[]
+) => {
+  let newArr1: Cars[] = [];
+  let newArr2: Cars[] = [];
+  let newArr3: Cars[] = [];
+  let newArr4: Cars[] = [];
+
+  checkFilter.map((el) => {
+    if (el.model) {
+      afterArr.map((item) => {
+        if (el.model === item.model) {
+          const checkEl = newArr1.includes(item);
+          if (checkEl === false) {
+            newArr1.push(item);
+          }
         }
+      });
+    }
+  });
 
-    })
+  let timeArr = newArr1.length === 0 ? afterArr : newArr1;
 
-    
-    return resultArr;
+  checkFilter.map((el) => {
+    if (el.color) {
+      timeArr.map((item) => {
+        if (el.color === item.color) {
+          const checkEl = newArr2.includes(item);
+          if (checkEl === false) {
+            newArr2.push(item);
+          }
+        }
+      });
+    }
+  });
 
-}
+  let timeArr2: Cars[] = [];
+  
+  if (newArr2.length === 0 && newArr1.length === 0) {
+    timeArr2 = afterArr;
+  }
+
+  if (newArr2.length === 0 && newArr1.length != 0) {
+    timeArr2 = newArr1;
+  }
+
+  checkFilter.map((el) => {
+    if (el.status) {
+      timeArr2.map((item) => {
+        if (el.status === item.status) {
+          const checkEl = newArr3.includes(item);
+          if (checkEl === false) {
+            newArr3.push(item);
+          }
+        }
+      });
+    }
+  });
+
+  let timeArr3 = newArr3.length === 0 ? afterArr : newArr3;
+
+  checkFilter.map((el) => {
+    if (el.typeCar) {
+      timeArr3.map((item) => {
+        if (el.typeCar === item.typeCar) {
+          const checkEl = newArr4.includes(item);
+          if (checkEl === false) {
+            newArr4.push(item);
+          }
+        }
+      });
+    }
+  });
+
+  return newArr4;
+};
